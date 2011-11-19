779811072119269dda4cca39a09b6ac9a59fd59a
Nicolas Brousse <pro@nicolas-brousse.fr>
2011-11-19 22:38:45 +0100

    Cleaning

--- js/app/start_page.js
+++ js/app/start_page.js
@@ -1,9 +1,5 @@
 var StartPage = {
-  initWindow: function()
-  {
-    
-  }
-  ,init: function(appWindow)
+  init: function(appWindow)
   {
     var theFunction = function () {
       // $('#home').css("margin-top", (($(window).height() - $('#home').outerHeight()) / 2) + $(window).scrollTop() + "px");
@@ -13,7 +9,7 @@ var StartPage = {
       $('#home div').css("top", (($('#home').height() - $('#home div').outerHeight()) / 2) + $('#home').scrollTop() + "px");
       $('#home div').css("left", (($('#home').width() - $('#home div').outerWidth()) / 2) + $('#home').scrollLeft() + "px");
     }
-    theFunction();
+    $(document).ready(theFunction);
     $(window).resize(theFunction);
   }
 }
