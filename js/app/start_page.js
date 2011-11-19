2569f29f2281e68b49904f226d56cb9a16eae248
jlima <lima.juliette@laposte.net>
2011-11-19 02:07:17 +0100

    Revert c4d3db610ebd1fc72d8a0efe5b8d5560d23d6054^..HEAD

--- js/app/start_page.js
+++ js/app/start_page.js
@@ -3,6 +3,6 @@ var StartPage = {
   {
     document.getElementById("home").style.width = appWindow.width + "px";
     document.getElementById("home").style.marginTop = appWindow.height /2-150 + "px";
-    //document.getElementById("content").style.marginTop = appWindow.height /2 - 150 + "px";
+    document.getElementById("content").style.marginTop = appWindow.height /2 - 150 + "px";
   }
 }
